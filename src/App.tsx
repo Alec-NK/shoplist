aa844cdf60785ccc5b58e2e372b6f9a6b7821ac0
Alec-NK <alec.arquivos@gmail.com>
2023-02-14 19:31:12 -0300

    feat: theme, chakra ui e styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import ShoppingList from "./pages/ShoppingList";
+
+import { DefaultTheme } from "./styles/theme";
+import GlobalStyle from "./styles/global";
 
 function App() {
-  return <div className="App">asd</div>;
+  return (
+    <ChakraProvider>
+      <ThemeProvider theme={DefaultTheme}>
+        <ShoppingList />
+        <GlobalStyle />
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,5 @@
+export const DefaultTheme = {
+  sea_green: "#37BD6B",
+  dark_sea_green: "#31A85F",
+  white_gray: "#f4f4f4",
+};
