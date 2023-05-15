6f93c192264b35217be39ae775b8ce63e31a127b
Alec-NK <alec.arquivos@gmail.com>
2023-05-14 22:05:06 -0300

    feat: add colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,8 @@ export const DefaultTheme = {
   sea_green: "#37BD6B",
   dark_sea_green: "#31A85F",
   green: "#44be8d",
+  red: "#f75c5c",
+  gray: "#C1C7D0",
+  light_gray: "#EBECF0",
   white_gray: "#f4f4f4",
 };
